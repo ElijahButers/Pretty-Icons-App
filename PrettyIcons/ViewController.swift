0b991de299ece7d969956935411383c311defca2
ElijahButers <Elijahbuters@gmail.com>
2016-08-02 14:45:50 +0300

    Updated code in ViewController class that creates a variable containing the icon sets.

--- PrettyIcons/ViewController.swift
+++ PrettyIcons/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var icons = [Icon]()
+    var iconSets = [IconSet]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
