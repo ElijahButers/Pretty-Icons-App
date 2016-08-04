12865f02f35214d58a0b32c2c5a50f0ad738ba22
ElijahButers <Elijahbuters@gmail.com>
2016-08-04 13:35:12 +0300

    Connected the table view to ViewController as outlet.

--- PrettyIcons/ViewController.swift
+++ PrettyIcons/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var iconSets = [IconSet]()
 
     override func viewDidLoad() {
