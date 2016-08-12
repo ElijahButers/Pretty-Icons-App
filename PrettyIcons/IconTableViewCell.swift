1d48186161467d05a0cec9597c7b78f7d1f75b13
ElijahButers <Elijahbuters@gmail.com>
2016-08-12 16:59:33 +0300

    Added IconTableViewCell, subclass of table view cell.

---
+++ PrettyIcons/IconTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IconTableViewCell.swift
+//  PrettyIcons
+//
+//  Created by User on 8/12/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class IconTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
